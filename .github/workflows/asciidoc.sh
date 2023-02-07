e305e8923f91de0641aad98d35beb5130e97cb99
Petr Kovar <pkovar@redhat.com>
2023-02-07 15:55:59 +0100

    CI: Also push README

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -8,3 +8,5 @@ CURRENT_PATH=`pwd`
 asciidoctor -D ${CURRENT_PATH}/output/ -o index.html community/index.adoc
 
 asciidoctor-pdf -D ${CURRENT_PATH}/output/ -o rpm-packaging-guide.pdf  community/index.adoc
+
+cp README.md ${CURRENT_PATH}/output/
